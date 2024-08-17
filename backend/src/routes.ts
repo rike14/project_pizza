b9c06b997c3612bba4145adb350cef1cf6a35e8b
Henrique Kronhardt <henriquemk00@gmail.com>
2024-08-17 15:51:51 -0300

    route post create category product, service and controller category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
-import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router()
 
@@ -12,5 +14,8 @@ const router = Router()
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// CATEGORIES ROUTES
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router };

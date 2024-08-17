8d9ee719f8dac8bb57c1af71618672064e1343d3
Henrique Kronhardt <henriquemk00@gmail.com>
2024-08-17 15:40:56 -0300

    return token user, add user_id request

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,10 +3,14 @@ import { Router } from "express";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 const router = Router()
 
+// USER ROUTES
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
-router.get('/me', new DetailUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 export { router };

eaf18ff283cdc4bc5c92948df5823e4736ce2217
Henrique Kronhardt <henriquemk00@gmail.com>
2024-08-24 13:23:21 -0300

    udpate loading

--- web/src/app/components/loading/loading.tsx
+++ web/src/app/components/loading/loading.tsx
@@ -3,9 +3,22 @@ import styles from './styles.module.scss';
 export default function Loading() {
   return (
     <>
-      <div className={styles.skeletonContainer}>
-        <p className={styles.skeleton}>Loading...</p>
+      <div className={styles.spinnerContainer}>
+        <span className={styles.spinner}></span>
+        <span className={styles.spinnerBackground}></span>
       </div>
+      <div className={styles.loader}>
+        <div className={styles.flipping}>
+            <div className={styles.card}>L</div>
+            <div className={styles.card}>o</div>
+            <div className={styles.card}>a</div>
+            <div className={styles.card}>d</div>
+            <div className={styles.card}>i</div>
+            <div className={styles.card}>n</div>
+            <div className={styles.card}>g</div>
+            <div className={styles.card}>...</div>
+        </div>
+    </div>
     </>
   )
 }

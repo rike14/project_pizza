f9f43faff24766186a04252c1487550f1745dd9b
rike14 <henriquemk00@gmail.com>
2024-08-26 16:34:34 -0300

    remove await cookieClient

--- web/src/lib/cookieClient.ts
+++ web/src/lib/cookieClient.ts
@@ -1,7 +1,7 @@
 import { getCookie } from "cookies-next"
 
-export async function getCookieClient() {
-    const token = await getCookie("session")
+export function getCookieClient() {
+    const token = getCookie("session")
 
     return token
 }

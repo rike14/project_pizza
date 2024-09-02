0d0a591503b96f48aadc8ba315b03c1e6562a5b5
Henrique Kronhardt <henriquemk00@gmail.com>
2024-09-02 01:35:31 -0300

    back next config

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -7,16 +7,8 @@ const nextConfig = {
                 protocol: 'http',
                 hostname: 'localhost',
                 port: '3333',
-            },
-            {
-                protocol: 'https',
-                hostname: process.env.NEXT_PUBLIC_API_URL,
-                port: '',
             }
         ]
-    },
-    env: {
-        key: "NEXT_PUBLIC_API_URL"
     }
 };
 

99f49103653cd15a4d8053b3578aa07ef01eccd6
Henrique Kronhardt <henriquemk00@gmail.com>
2024-09-02 01:41:54 -0300

    add https url next config

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'apisujeitopizza.vercel.app',
+                hostname: 'https://apisujeitopizza.vercel.app',
             }
         ]
     }

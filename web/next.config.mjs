a87e43ad5af53ed927832f31ada072db143f9305
Henrique Kronhardt <henriquemk00@gmail.com>
2024-09-02 01:26:25 -0300

    update next config

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: process.env.API_URL,
+                hostname: '**',
             },
         ],
     }
